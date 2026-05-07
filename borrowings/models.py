cb19e23387cae2cdc6bfa6e876d66cf27ad4dba8
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 19:47:04 +0100

    Add validation for book Borrowing model and serializer

--- borrowings/models.py
+++ borrowings/models.py
@@ -28,5 +28,19 @@ class Borrowing(models.Model):
             ),
         ]
 
+    @staticmethod
+    def validate_book_inventory(inventory: int, error_to_raise: Exception) -> None:
+        if inventory == 0:
+            raise error_to_raise("This book is currently unavailable (inventory is 0).")
+
+    @staticmethod
+    def validate_return_date(expected_return_date, borrow_date, error_to_raise: Exception) -> None:
+        if expected_return_date <= borrow_date:
+            raise error_to_raise("expected_return_date must be after borrow_date.")
+
+    def clean(self):
+        Borrowing.validate_book_inventory(self.book.inventory, ValueError)
+        Borrowing.validate_return_date(self.expected_return_date, self.borrow_date, ValueError)
+
     def __str__(self):
         return f"{self.user} borrowed {self.book} on {self.borrow_date}"
