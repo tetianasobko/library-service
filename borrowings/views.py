9cc6bf3daf9758f88d45e648a4321354c331e2b9
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 19:47:28 +0100

    Implement borrowing creation functionality

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,12 +2,24 @@ from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingReadSerializer
+from borrowings.serializers import BorrowingCreateSerializer, BorrowingReadSerializer
 
 
-class BorrowingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-    serializer_class = BorrowingReadSerializer
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Borrowing.objects.filter(user=self.request.user).select_related("book", "user")
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
