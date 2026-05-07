8086d21e1774770e51a13596cec973aa1e77492b
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 18:36:35 +0100

    Implement BorrowingViewSet for list and detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingReadSerializer
+
+
+class BorrowingViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    serializer_class = BorrowingReadSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Borrowing.objects.filter(user=self.request.user).select_related("book", "user")
