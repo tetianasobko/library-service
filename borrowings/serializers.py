82e49abec51b0f9ca54850a4ffa7f5922d1bde03
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 19:45:33 +0100

    Implement BorrowingCreateSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
@@ -17,3 +18,16 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "book")
+
+    @transaction.atomic
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save(update_fields=["inventory"])
+        return super().create(validated_data)
