d0aa72b3f1e270c18cb173629e7a9789134dd665
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 22:34:05 +0100

    Implement return borrowing functionality

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,10 @@
-from rest_framework import mixins, viewsets
+import datetime
+
+from django.db import transaction
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingCreateSerializer, BorrowingReadSerializer
@@ -23,3 +28,22 @@ class BorrowingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="return")
+    @transaction.atomic
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_date is not None:
+            return Response(
+                {"detail": "This borrowing has already been returned."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        borrowing.actual_return_date = datetime.date.today()
+        borrowing.save(update_fields=["actual_return_date"])
+
+        borrowing.book.inventory += 1
+        borrowing.book.save(update_fields=["inventory"])
+
+        return Response(BorrowingReadSerializer(borrowing).data, status=status.HTTP_200_OK)
