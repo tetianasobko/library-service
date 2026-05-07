9e199c603d008c71ef1e79235ff15ac1bc91a295
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-07 18:35:24 +0100

    Add constraints to Borrowing model for validation

--- borrowings/models.py
+++ borrowings/models.py
@@ -13,5 +13,20 @@ class Borrowing(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(expected_return_date__gt=models.F("borrow_date")),
+                name="expected_return_after_borrow",
+            ),
+            models.CheckConstraint(
+                condition=(
+                    models.Q(actual_return_date__isnull=True)
+                    | models.Q(actual_return_date__gt=models.F("borrow_date"))
+                ),
+                name="actual_return_after_borrow",
+            ),
+        ]
+
     def __str__(self):
         return f"{self.user} borrowed {self.book} on {self.borrow_date}"
