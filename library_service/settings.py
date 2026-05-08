811e4a023162c0cef2ec0bb8eb5139cc887bf77d
Tetiana Sobko <sobkotiana@gmail.com>
2026-05-08 15:19:57 +0100

    Update database settings to use PostgreSQL

--- library_service/settings.py
+++ library_service/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'library_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'library_db'),
+        'USER': os.getenv('POSTGRES_USER', 'library_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'library_pass'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
